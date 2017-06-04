a893eb53eb572f5b2f637088da4812082871d0f9
= <lvo@cirque.dk>
2017-06-04 15:37:41 +0200

    ..cmon

--- public/db/connection.js
+++ public/db/connection.js
@@ -3,6 +3,6 @@
 const connection = require(__base+'/../configuration/config').db
 
 const mongoose = require('mongoose')
-mongoose.createConnection(connection.uri, connection.options)
+mongoose.connect(connection.uri, connection.options)
 
 mongoose.Promise = require('bluebird')
