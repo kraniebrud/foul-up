2f724680e8fc96b59ddc2f86c5d55aab3befc98c
= <lvo@cirque.dk>
2017-07-13 11:04:28 +0200

    cant spend more time on this, made some handy console logs in the code and stuff

--- public/admin/static/js/svmde.js
+++ public/admin/static/js/svmde.js
@@ -1,16 +1,164 @@
 (function (){
 	
-	var ed
+	function getSelection() {
+		// return window.getSelection().toString()
+		return window.getSelection()
+	}
+
+	function textContainsANewLine (txt) {
+		return txt.search('\n') !== -1
+	}
+
+	function execInsert(exTxt){
+		console.log('execInsert')
+		// document.execCommand('insertText', false, exTxt)
+	}
+
+	function buildElemsWithCommands (txtElem, commandList){
+		var loadedTextareaValue = txtElem.value
+
+		var mdContainer = document.createElement('div')
+		var mdToolbar = document.createElement('ul')
+		var mdEditor = document.createElement('div')
+		var mdTextarea = document.createElement('textarea')
+
+		var containerIdClass = 'svmde'+Date.now()
+		mdContainer.className='svmde-container '+containerIdClass
+		mdToolbar.className='svmde-toolbar'
+		mdEditor.className = 'svmde-editor'
+		mdTextarea.className = 'svmde-textarea'
+
+		commandList.forEach(function(cmd){
+			var cmdLi = document.createElement('li')
+			cmdLi.dataset.cmd = cmd
+			mdToolbar.appendChild(cmdLi)
+		})
+
+		mdEditor.setAttribute('contenteditable', true)
+		
+		mdContainer.appendChild(mdToolbar)
+		mdContainer.appendChild(mdEditor)
+		mdContainer.appendChild(mdTextarea)
+
+		txtElem.outerHTML = mdContainer.outerHTML
+
+		var container = document.querySelector('.'+containerIdClass)
+		var toolbar = container.querySelector('.svmde-toolbar')
+		var textarea = container.querySelector('.svmde-textarea')
+		var editor = container.querySelector('.svmde-editor')
+		textarea.value = loadedTextareaValue
+		editor.innerHTML = loadedTextareaValue
+		return {
+			container: container,
+			toolbar: toolbar,
+			textarea: textarea,
+			editor: editor
+		}
+	}
+
+	if(typeof window.getSelection !== undefined){ //only if able to getSelection within browser 'window'
+		document.querySelectorAll('.svmde').forEach(
+			function(txtareaElem){
+				newEditor(txtareaElem)
+			}
+		)
+	}
 
 	function newEditor (loadedTextareaElem){
-		ed = new Editor(loadedTextareaElem)
+		var ed = new Editor(loadedTextareaElem)
 		var toolbar = ed.elem.toolbar
-		var textarea = ed.elem.textarea		
+		var textarea = ed.elem.textarea
+		var editor = ed.elem.editor
+		
 		toolbar.addEventListener('click', function(event){
-			if(ed.isTextareaActivated){
+			alert('click')
+			//if(ed.isTextareaActivated){
 				insert(event.target.dataset.cmd)
+			//}
+		})
+
+		function markDescription(symb, description, newline) {
+			var startSelect = ed.selectionStart+symb.length
+			var endSelect  = ed.selectionStart+description.length+symb.length
+			if(newline) endSelect++
+			editor.setSelectionRange(startSelect, endSelect)
+		}
+
+		function insert (cmd){
+			textarea.focus()
+			var selStart = textarea.selectionStart
+			var selEnd = textarea.selectionEnd
+			ed.selectionStart = selStart
+			ed.selectionEnd = textarea.selectionEnd
+
+
+
+
+			var insTxt = textarea.value.substring(selStart, selEnd)
+			
+			console.log(editor.innerHTML.substring(selStart, selEnd))
+			
+			switch(cmd) {
+				case 'h1': 
+					wrappingTag('#', insTxt, 'Headline text')
+					break;
+				case 'b':
+					wrappingTag(['**'], insTxt, 'Bold text')
+					break;
+				case 'u':
+					wrappingTag(['*'], insTxt, 'Underlined text')
+					break;
+				case 'ul':
+					multilineTag('*', insTxt, 'Listed text')
+					break;
+				case 'br':
+					nonWrappingTag('<br>\n', insTxt)
+					break;
+				case 'hr':
+					nonWrappingTag('\n---\n', insTxt)
 			}
-		})		
+		}
+
+		//<tag></tag> is a wrapping tag
+		function wrappingTag(symbol, sText, description){
+			var noSelectedText = sText.length === 0
+			if(sText === description) return
+			if(description !== undefined && noSelectedText === true){
+				sText = description
+			}
+			if (typeof symbol === 'object'){
+				var symb = symbol[0]
+				execInsert(symb+sText+symb)
+			} 
+			else {
+				var firstChar = sText.charAt(0)
+				var symb = firstChar !== symbol ? symbol+' ' : symbol
+				var tValue = ed.elem.textarea.value
+				var charBeforeCursor = tValue.substring(ed.selectionStart, ed.selectionStart-1)
+				execInsert(symb+sText)
+			}
+			if(noSelectedText){
+				markDescription(symb, sText)
+			}
+		}
+
+		function nonWrappingTag(symbol, sText){
+			execInsert(sText+symbol)
+		}
+
+		function multilineTag(symbol, sText, description) {
+			if(textContainsANewLine(sText) === false) {
+				wrappingTag(symbol, sText, description)
+			}
+			else{
+				var lines = sText.split('\n')
+				for(var i = 0; lines.length > i; i++){
+					var newline = '\n'
+					if(i === lines.length-1) newline = ''
+					wrappingTag(symbol, lines[i]+newline)
+				}
+			}
+		}
 	}
 
 	 function Editor (txtElem) {
@@ -23,143 +171,6 @@
 			self.elem.textarea.removeEventListener('click', setActivateTextArea)
 		}
 		this.elem.textarea.addEventListener('click', setActivateTextArea)
-
-		this.elem.container.addEventListener('mouseenter', function () {
-			//swaps active editor on mouseenter)
-			ed = self
-		})
-	}
-
-	document.querySelectorAll('.svmde').forEach(
-		function(txtareaElem){
-			newEditor(txtareaElem)
-		}
-	)
-
-	function buildElemsWithCommands (txtElem, commandList){
-		var loadedTextareaAttributes = txtElem.attributes
-		var loadedTextareaValue = txtElem.value
-
-		var mdContainer = document.createElement('div')
-		var mdToolbar = document.createElement('ul')
-		var mdTextarea = document.createElement('textarea')
-
-		//keep the originated attributes on textarea
-		for(var i = 0; loadedTextareaAttributes.length > i; i++){
-			var attr = loadedTextareaAttributes[i]
-			mdTextarea.setAttribute(attr.nodeName, attr.nodeValue)
-		}
-
-		var containerIdClass = 'svmde'+Date.now()
-		mdContainer.className='svmde-container '+containerIdClass
-		mdToolbar.className='svmde-toolbar'
-
-		commandList.forEach(function(cmd){
-			var cmdLi = document.createElement('li')
-			cmdLi.dataset.cmd = cmd
-			mdToolbar.appendChild(cmdLi)
-		})
-		
-		mdContainer.appendChild(mdToolbar)
-		mdContainer.appendChild(mdTextarea)
-
-		txtElem.outerHTML = mdContainer.outerHTML
-
-		var container = document.querySelector('.'+containerIdClass)
-		var toolbar = container.firstElementChild
-		var textarea = container.lastElementChild
-
-		textarea.value = loadedTextareaValue
-		return {
-			container: container,
-			toolbar: toolbar,
-			textarea: textarea
-		}
-	}
-
-	function getSelection() {
-		return window.getSelection().toString()
-	}
-
-	function textContainsANewLine (txt) {
-		return txt.search('\n') !== -1
-	}
-
-	function execInsert(exTxt){
-		document.execCommand('insertText', false, exTxt)
-	}
-
-	function markDescription(symb, description, newline) {
-		var startSelect = ed.selectionStart+symb.length
-		var endSelect  = ed.selectionStart+description.length+symb.length
-		if(newline) endSelect++
-		ed.elem.textarea.setSelectionRange(startSelect, endSelect)
-	}
-
-	//<tag></tag> is a wrapping tag
-	function wrappingTag(symbol, sText, description){
-		var noSelectedText = sText.length === 0
-		if(sText === description) return
-		if(description !== undefined && noSelectedText === true){
-			sText = description
-		}
-		if (typeof symbol === 'object'){
-			var symb = symbol[0]
-			execInsert(symb+sText+symb)
-		} 
-		else {
-			var firstChar = sText.charAt(0)
-			var symb = firstChar !== symbol ? symbol+' ' : symbol
-			var tValue = ed.elem.textarea.value
-			var charBeforeCursor = tValue.substring(ed.selectionStart, ed.selectionStart-1)
-			execInsert(symb+sText)
-		}
-		if(noSelectedText){
-			markDescription(symb, sText)
-		}
-	}
-
-	function nonWrappingTag(symbol, sText){
-		execInsert(sText+symbol)
-	}
-
-	function multilineTag(symbol, sText, description) {
-		if(textContainsANewLine(sText) === false) {
-			wrappingTag(symbol, sText, description)
-		}
-		else{
-			var lines = sText.split('\n')
-			for(var i = 0; lines.length > i; i++){
-				var newline = '\n'
-				if(i === lines.length-1) newline = ''
-				wrappingTag(symbol, lines[i]+newline)
-			}
-		}
-	}
-
-	function insert (cmd){
-		ed.elem.textarea.focus()
-		ed.selectionStart = ed.elem.textarea.selectionStart
-		var insTxt = getSelection()
-		switch(cmd) {
-			case 'h1': 
-				wrappingTag('#', insTxt, 'Headline text')
-				break;
-			case 'b':
-				wrappingTag(['**'], insTxt, 'Bold text')
-				break;
-			case 'u':
-				wrappingTag(['*'], insTxt, 'Underlined text')
-				break;
-			case 'ul':
-				multilineTag('*', insTxt, 'Listed text')
-				break;
-			case 'br':
-				nonWrappingTag('<br>\n', insTxt)
-				break;
-			case 'hr':
-				nonWrappingTag('\n---\n', insTxt)
-		}
 	}
 
 })()
