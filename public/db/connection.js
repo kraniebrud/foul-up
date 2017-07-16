768993fe8393864046d7425e2c3a484cc2938e5e
= <lvo@cirque.dk>
2017-07-16 21:27:16 +0200

    ...

--- public/db/connection.js
+++ public/db/connection.js
@@ -1,12 +1,9 @@
-const {
-	DB_HOST, DB_NAME, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_AUTH_NAME,
-	COOKIE_NAME, COOKIE_PASSWORD, COOKIE_TTL
-} = process.env
+const {DB_HOST, DB_NAME, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_AUTH_NAME} = process.env
 
 const mongoose = require('mongoose')
 
 mongoose.connect(
-	`mongodb://${DB_HOST}/${DB_NAME}:${DB_PORT}`, {
+		`mongodb://${DB_HOST}/${DB_NAME}:${DB_PORT}`, {
 		server: { poolSize: 5 },
 		user: DB_USERNAME,
 		pass: DB_PASSWORD,
